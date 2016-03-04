9d2e9b0f52f171d7bd3b8f4178b18ded4346d0da
d.larchikov <d.larchikov@gmail.com>
2016-03-04 16:04:08 +0300

    web update

--- web/js/main.js
+++ web/js/main.js
@@ -1,7 +1,4 @@
-var io = io('http://' + window.location.hostname + ':8101');
-
-io.on('connection', function (socket) {
-    socket.on('message', function (data) {
-        console.log(data);
-    })
+var socket = io('http://' + window.location.hostname + ':8099');
+socket.on('stats.update', function (data) {
+    $('#testResult').prepend(data + "<hr>");
 });
