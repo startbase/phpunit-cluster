098fe217fc7552056ff1e0a43e7d60315054f8ca
v.sheremetov <v.sheremetov@b2b-center.ru>
2016-03-15 17:02:15 +0300

    forker refactor 2

--- client_runner.js
+++ client_runner.js
@@ -1,32 +1,3 @@
-var forker = require('./libs/forker.js');
-var spawn = require('child_process').spawn;
-var git = require('simple-git')();
+var ClientForker = require('./libs/client-forker.js');
 
-var CRunner = function() {
-
-    this.updateClient = function(callback) {
-        git.fetch('origin').pull(function() {
-            console.log('Repository has been updated');
-            spawn('npm', ['install']).on('close', function() {
-                console.log('npm install completed');
-                callback();
-            });
-        });
-    };
-
-    this.getArgv = function() {
-        var argv = process.argv;
-        argv.splice(1, 1);
-        return argv;
-    };
-
-    this.run = function() {
-        var self = this;
-        var f = new forker(this.getArgv(), function(callback) {
-            self.updateClient(callback);
-        });
-        f.restartApp();
-    };
-};
-
-(new CRunner()).run();
+(new ClientForker()).run();
---
+++ libs/client-forker.js
@@ -0,0 +1,32 @@
+var forker = require('./forker.js');
+var spawn = require('child_process').spawn;
+var git = require('simple-git')();
+
+var ClientForker = function() {
+
+    this.updateClient = function(callback) {
+        git.fetch('origin').pull(function() {
+            console.log('Repository has been updated');
+            spawn('npm', ['install']).on('close', function() {
+                console.log('npm install completed');
+                callback();
+            });
+        });
+    };
+
+    this.getArgv = function() {
+        var argv = process.argv;
+        argv.splice(1, 1);
+        return argv;
+    };
+
+    this.run = function() {
+        var self = this;
+        var f = new forker(this.getArgv(), function(callback) {
+            self.updateClient(callback);
+        });
+        f.restartApp();
+    };
+};
+
+module.exports = ClientForker;
