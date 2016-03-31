1c157b083a94520398c9a1b1e6c66fd0dc3d0402
v.sheremetov <v.sheremetov@b2b-center.ru>
2016-03-31 15:30:20 +0300

    client runner refactor

--- client_runner.js
+++ client_runner.js
@@ -1,3 +1,33 @@
-var ClientForker = require('./libs/client-forker.js');
+var forker = require('./libs/forker.js');
+var spawn = require('child_process').spawn;
+var git = require('simple-git')();
+
+var ClientForker = function(sh) {
+    this.client_script = sh || 'client.js';
+
+    this.updateClient = function(callback) {
+        git.fetch('origin').pull(function() {
+            console.log('Repository has been updated');
+            spawn('npm', ['install']).on('close', function() {
+                console.log('npm install completed');
+                callback();
+            });
+        });
+    };
+
+    this.getArgv = function() {
+        var argv = process.argv;
+        argv[1] = this.client_script;
+        return argv;
+    };
+
+    this.run = function() {
+        var self = this;
+        var f = new forker(this.getArgv(), function(callback) {
+            self.updateClient(callback);
+        });
+        f.restartApp();
+    };
+};
 
 (new ClientForker()).run();
